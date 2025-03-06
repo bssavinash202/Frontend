37173e5ec9ee8b0cfadca7e349db958130eaeeee
bssavinash202 <bssavinash202@gmail.com>
2025-03-06 15:52:28 +0530

    Crud Operation using Unique ID

--- Task13-heroines/script.js
+++ Task13-heroines/script.js
@@ -1,70 +1,79 @@
 const heroines = [
     {
+      id:101,
       name: "Samantha Ruth Prabhu",
       age: 36,
       numberOfMovies: 50,
       latestMovie: "Kushi"
     },
     {
+      id:265,
       name: "Rashmika Mandanna",
       age: 28,
       numberOfMovies: 15,
       latestMovie: "Animal"
     },
     {
+      id:398,
       name: "Pooja Hegde",
       age: 33,
       numberOfMovies: 20,
       latestMovie: "Guntur Kaaram"
     },
     {
+      id:454,
       name: "Keerthy Suresh",
       age: 31,
       numberOfMovies: 25,
       latestMovie: "Dasara"
     },
     {
+      id:523,
       name: "Sai Pallavi",
       age: 32,
       numberOfMovies: 18,
       latestMovie: "Gargi"
     },
     {
+      id:634,
       name: "Tamannaah Bhatia",
       age: 34,
       numberOfMovies: 65,
       latestMovie: "Bhola Shankar"
     },
     {
+      id:734,
       name: "Anushka Shetty",
       age: 42,
       numberOfMovies: 48,
       latestMovie: "Miss Shetty Mr Polishetty"
     },
     {
+      id:821,
       name: "Kajal Aggarwal",
       age: 39,
       numberOfMovies: 60,
       latestMovie: "Bhagavanth Kesari"
     },
     {
+      id:954,
       name: "Nabha Natesh",
       age: 28,
       numberOfMovies: 10,
       latestMovie: "Maestro"
     }
   ];
-let editIndex = -1;
   const display=()=>{
     let returnval = heroines.reduce((acc,val,ind)=>{
         let tr=`
             <tr>
+                <td>${val.id}</td>
                 <td>${val.name}</td>
                 <td>${val.age}</td>
                 <td>${val.numberOfMovies}</td>
                 <td>${val.latestMovie}</td>
                 <td><button onclick="deleteItem(${ind})" class="delete"><i class="fa-solid fa-trash"></i><span> Delete</span></button></td>
-                <td><button onclick="EditItem(${ind})"class="edit"><i class="fa-solid fa-pen-to-square"></i><span> Edit</span></button></td>
+                <td><button onclick="EditItem(${val.id})"class="edit"><i class="fa-solid fa-pen-to-square"></i><span> Edit</span></button></td>
             </tr>
         `
         acc+=tr
@@ -74,6 +83,7 @@ let editIndex = -1;
     },"")
     let table = `
         <tr>
+            <th>Serial No</th>
             <th>Name</th>
             <th>Age</th>
             <th>Movies Acted</th>
@@ -90,14 +100,20 @@ let editIndex = -1;
     heroines.splice(index,1)
     display()
   }
-
   const Add=()=>{
-    
-    let name = document.getElementById('name').value
-    let age = document.getElementById('age').value
-    let Movie = document.getElementById('movies').value
-    let latest =document.getElementById('latest').value
+    let id = document.getElementById('id').value.trim()
+    let name = document.getElementById('name').value.trim()
+    let age = document.getElementById('age').value.trim()
+    let Movie = document.getElementById('movies').value.trim()
+    let latest =document.getElementById('latest').value.trim()
+    let ageNum = Number(age)
+    let MovieNum=Number(Movie)
+    let idNum=Number(id)
+    if(name===""||latest===""||isNaN(ageNum)||isNaN(MovieNum)||isNaN(idNum)){
+      alert("Enter all the Fields")
+    }else{
     let obj = {
+        id:id,
         name:name,
         age:age,
         numberOfMovies:Movie,
@@ -106,38 +122,63 @@ let editIndex = -1;
     heroines.push(obj)
     display()
   }
-  const EditItem=(index)=>{
+}
+  const EditItem=(id)=>{
     document.getElementById('addBtn').style.display="none";
     document.getElementById('editBtn').style.display="block";
+    document.getElementById('id').setAttribute('disabled','true')
+    let idx = document.getElementById('id')
     let namex = document.getElementById('name')
     let agex = document.getElementById('age')
     let Moviex = document.getElementById('movies')
     let latestx =document.getElementById('latest')
-    editIndex = index;
-    namex.value=heroines[index].name
-    agex.value=heroines[index].age
-    Moviex.value = heroines[index].numberOfMovies
-    latestx.value= heroines[index].latestMovie
-    console.log(editIndex)
-  }
-  const Edit=()=>{
-    if(editIndex==-1){
-      return;
+    let heroine = heroines.find((val)=>{
+      if(val.id===id){
+        return val;
+      }
+    })
+    console.log(heroine)
+    
+    if(heroine){
+    idx.value = heroine.id
+    namex.value=heroine.name
+    agex.value=heroine.age
+    Moviex.value = heroine.numberOfMovies
+    latestx.value= heroine.latestMovie
     }
+    document.getElementById('editBtn').onclick=(()=>Edit(id))
+    
+  }
+  const Edit=(id)=>{
+    let idx = document.getElementById('id').value
     let namex = document.getElementById('name').value
     let agex = document.getElementById('age').value
     let Moviex = document.getElementById('movies').value
     let latestx =document.getElementById('latest').value
-   
-    
     let obj={
-       name: namex,
-        age:agex,
-        numberOfMovies:Moviex,
-        latestMovie:latestx
-    }
-    heroines[editIndex]=obj;
-    editIndex =-1;
-    display()
+      id:idx,
+      name: namex,
+      age:agex,
+      numberOfMovies:Moviex,
+      latestMovie:latestx
   }
-  
+    let index=heroines.findIndex((val)=>{
+      if(val.id===id){
+        return val
+      }
+    })
+    console.log(index)
+    if(index){
+      heroines[index]=obj
+    }
+    alert("Successfully Updated")
+    display();
+    clearInputs();
+  }
+  const clearInputs = ()=>{
+    document.getElementById('id').value=""
+    document.getElementById('name').value=""
+    document.getElementById('age').value=""
+    document.getElementById('movies').value=""
+    document.getElementById('latest').value=""
+  }
